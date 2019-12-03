7f5f3379310822190d89e2399465d7e0a00ef5d2
maheenahmed <50530906+maheenahmed@users.noreply.github.com>
2019-12-03 18:55:43 +0500

    Add files via upload
    difficulty level : beginner 
    board used: arduino uno
    A project for school science exhibition. This is a simple code which uses LED as water level indicator.

---
+++ Water_motor.ino
@@ -0,0 +1,29 @@
+
+
+void setup()
+{
+  pinMode(7, OUTPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  digitalWrite(7, LOW); 
+
+}
+
+
+void loop() 
+{
+
+  if (digitalRead (8)==HIGH)
+  {
+  digitalWrite(7, HIGH);
+    while (digitalRead(9)==1)
+    {
+      delay (500); 
+    }
+  digitalWrite(7, LOW); 
+
+  
+  }
+  
+  
+}
